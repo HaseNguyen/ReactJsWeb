a141a0613524e23fe891add221d873a4108c0aaf
Lan Nguyen <46945964+HaseNguyen@users.noreply.github.com>
2021-11-10 20:14:11 +0900

    First Push

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import PageWrapper from './components/PageWrapper';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+//Pages
+import Home from './components/Pages/Home'
+import About from './components/Pages/About'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <PageWrapper>
+
+          <Route
+            exact={true}
+            path="/"
+            component={Home}
+          />
+          <Route
+            path="/a"
+            render={() => {
+              return (
+                <h1>I am the /a Route</h1>
+              )
+            }}
+          />
+
+          <Route
+            path="/ab"
+            render={() => {
+              return (
+                <h1>I am the /ab Route</h1>
+              )
+            }}
+          />
+
+          <Route
+            path="/about"
+            component={About}
+          />
+
+
+        </PageWrapper>
+      </Router>
+    );
+  }
+}
+
+export default App;
